3b91c35dc57bfcdb1a6bd892804730200654aec8
harry0000 <zan00753@nifty.ne.jp>
2017-01-30 17:06:51 +0900

    Initial commit

---
+++ src/main/g8/src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+        assertThat(true, is(true));
+    }
+
+}
